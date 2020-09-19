9b5e0b9477b077d197f8f7a6fb54ed2b6e4d7a52
itamar <itamarcu@gmail.com>
2020-09-19 11:36:47 +0300

    Turn to Face: updated link in comment

--- turn-to-face.js
+++ turn-to-face.js
@@ -5,8 +5,7 @@
 
  (does not persist if you reload)
 
- https://user-images.githubusercontent.com/6516621/93661815-9400ad00-fa63-11ea-81e4-567add173113.gif
- https://user-images.githubusercontent.com/6516621/93661817-95ca7080-fa63-11ea-87cd-133eb5d576fc.gif
+ https://github.com/itamarcu/foundry-macros/blob/master/turn-to-face.js
  -- made by shem#0226
 */
 
