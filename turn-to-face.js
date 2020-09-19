debba2baaacd4cb5b6cf0be5064f970f27be7009
itamar <itamarcu@gmail.com>
2020-09-19 11:28:43 +0300

    Add comment to turn-to-face

--- turn-to-face.js
+++ turn-to-face.js
@@ -3,6 +3,8 @@
  Select one or more tokens to be the turners. Target one token to be the target.
  Whenever the turner or the target move, the turner will rotate to face the target.
 
+ (does not persist if you reload)
+
  https://user-images.githubusercontent.com/6516621/93661815-9400ad00-fa63-11ea-81e4-567add173113.gif
  https://user-images.githubusercontent.com/6516621/93661817-95ca7080-fa63-11ea-87cd-133eb5d576fc.gif
  -- made by shem#0226
