32b166d22003078a9043241d916b64f20eebb035
itamar <itamarcu@gmail.com>
2020-09-19 12:15:30 +0300

    critfail.js added

---
+++ critfail.js
@@ -0,0 +1,19 @@
+/*
+ --- critfail ---
+ Rolls and shows a result from the Critical Fail table
+
+ source:
+ https://github.com/itamarcu/foundry-macros/blob/master/critfail.js
+ suggested icon:
+ https://i.imgur.com/huPpJQf.png
+*/
+
+const table = game.tables.entities.find(t => t.name === "Critical Fail")
+
+const roll = table.roll()
+
+const rollPart = roll.roll
+const resultPart = Object.assign({}, roll.results[0]) // copy, otherwise we edit original table! :O
+resultPart.text = game.macros.getName("makeTableResultBold").renderContent(resultPart.text)
+
+table.draw({roll: rollPart , results: [resultPart]})
