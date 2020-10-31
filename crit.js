bf2d9374e97867a87abff66216f4fd1d8d1ecf8b
itamar <itamarcu@gmail.com>
2020-10-31 15:30:02 +0200

    crit.js better dependencies

--- crit.js
+++ crit.js
@@ -19,7 +19,34 @@ https://i.imgur.com/iw4sH39.png
 const input0 = args[0]
 let input = input0.toLowerCase().trim()
 
-const CRIT_TYPES = ['Bludgeoning', 'Piercing', 'Slashing', 'Fire', 'Acid', 'Cold', 'Force', 'Poison', 'Lightning', 'Necrotic', 'Psychic', 'Thunder', 'Radiant', 'Insanity', 'Minor Injury', 'Major Injury', 'Critical Fumble', 'Failure']
+const CRIT_TYPES = [
+  'Bludgeoning',
+  'Piercing',
+  'Slashing',
+  'Fire',
+  'Acid',
+  'Cold',
+  'Force',
+  'Poison',
+  'Lightning',
+  'Necrotic',
+  'Psychic',
+  'Thunder',
+  'Radiant',
+  'Insanity',
+  'Minor Injury',
+  'Major Injury',
+  'Critical Fumble',
+  'Failure',
+]
+
+const getDependency = async (entityMap, packName, entityName) => {
+  const existingEntity = entityMap.entities.find(t => t.name === entityName)
+  if (existingEntity) return existingEntity
+  const pack = game.packs.find(p => p.title === packName)
+  const id = pack.index.find(it => it.name === entityName)._id
+  return pack.getEntity(id)
+}
 
 let critType = null
 for (const crit of CRIT_TYPES) {
@@ -35,18 +62,17 @@ if (critType === null)
 if (critType === 'Failure' || critType === 'Critical Fumble')
   return '/critfail'
 
-let table = game.tables.entities.find(t => t.name === critType)
+const table = await getDependency(game.tables, 'Critical Hits', critType)
 if (!table) {
-  const pack = game.packs.find(p => p.title === "Critical Hits")
-  table = await pack.getEntity(pack.index.find(it => it.name === critType)._id)
-}
-if (!table) {
-  return `/error "Failed using ${input} crit - make sure you have the crit roll tables or compendium of them"`
+  return `/error "Failed using ${input} crit - make sure you have the Critical Hits compendium"`
 }
 
 const roll = table.roll()
 const rollPart = roll.roll
 const resultPart = Object.assign({}, roll.results[0]) // copy, otherwise we edit original table! :O
-resultPart.text = game.macros.getName('make-table-result-bold').renderContent(resultPart.text)
+const macro = await getDependency(
+  game.macros, 'itamacros', 'make-table-result-bold'
+)
+resultPart.text = macro.renderContent(resultPart.text)
 
 table.draw({ roll: rollPart, results: [resultPart] })
