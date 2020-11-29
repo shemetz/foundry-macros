8a91c2940d0fe887f86e6b22a6ca5782429cba1f
itamar <itamarcu@gmail.com>
2020-11-29 17:33:08 +0200

    Add toggle-hide and improve print-id

--- print-id.js
+++ print-id.js
@@ -9,7 +9,6 @@ suggested icon:
 https://i.imgur.com/iw4sH39.png
 */
 
-const controlled = canvas.tokens.controlled[0] ||
-  canvas.tiles.controlled[0]
-const id = controlled ? controlled.id : 'no tile selected'
-ChatMessage.create({ content: id, whisper: [game.user._id] })
+const controlled = canvas.tiles.controlled[0] || canvas.tokens.controlled[0]
+const id = controlled ? controlled.id : 'no tile/token selected'
+ChatMessage.create({content: id, whisper: [game.user._id]})
