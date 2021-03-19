6ea08c6f5f7e337be9664c01e5e6551b85d59c27
itamar <itamarcu@gmail.com>
2021-03-19 12:38:22 +0200

    Improve print-id

--- print-id.js
+++ print-id.js
@@ -1,6 +1,6 @@
 /*
 --- print-id ---
-Creates a private chat message with the ID of the currently selected token or tile.
+Creates a private chat message with the ID of the currently selected token/tile/wall.
 
 
 source:
@@ -9,6 +9,6 @@ suggested icon:
 https://i.imgur.com/iw4sH39.png
 */
 
-const controlled = canvas.tiles.controlled[0] || canvas.tokens.controlled[0]
+const controlled = canvas.tokens.controlled[0] || canvas.tiles.controlled[0] || canvas.walls.controlled[0]
 const id = controlled ? controlled.id : 'no tile/token selected'
 ChatMessage.create({content: id, whisper: [game.user._id]})
