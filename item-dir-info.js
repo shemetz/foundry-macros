256831d8f33c1670d1fe621e865ad2456590d998
itamar <itamarcu@gmail.com>
2021-03-20 11:14:37 +0200

    Improve item-dir-info.js, with custom font and ability to show character name

--- item-dir-info.js
+++ item-dir-info.js
@@ -13,16 +13,26 @@ suggested icon:
 https://i.imgur.com/iw4sH39.png
 */
 
-const itemNameOrId = args[0]
-const item = ItemDirectory.collection.getName(itemNameOrId) || ItemDirectory.collection.get(itemNameOrId)
-if (!item) return ui.notifications.error(`${itemNameOrId} is not an item name or ID that belongs to the item directory!`)
-const image = item.data.img === "icons/svg/mystery-man.svg" ? '' : `
-<div style="text-align: center;">
-  <img src=${item.data.img} style="width: 128px;"/>
-</div>`
-const content = `
-<h3>${item.name}</h3>
-${image}
-${item.data.data.description.value}
-`
-ChatMessage.create({content})
+const itemNameOrId = args[0], characterName = args[1]
+
+main()
+
+function main() {
+  const item = ItemDirectory.collection.getName(itemNameOrId) || ItemDirectory.collection.get(itemNameOrId)
+  if (!item) return ui.notifications.error(`${itemNameOrId} is not an item name or ID that belongs to the item directory!`)
+
+  const someoneFoundText = characterName ? `
+    <div style="line-height: 32px"><i>${characterName} found...</i></div>
+  ` : ``
+  const image = item.data.img === "icons/svg/mystery-man.svg" ? '' : `
+  <div style="text-align: center;">
+    <img src=${item.data.img} style="width: 128px;"/>
+  </div>`
+  const content = `
+    ${someoneFoundText}
+    <h3 style="font-family: 'Press Start',monospace">${item.name}</h3>
+    ${image}
+    ${item.data.data.description.value}
+  `
+  ChatMessage.create({content})
+}
